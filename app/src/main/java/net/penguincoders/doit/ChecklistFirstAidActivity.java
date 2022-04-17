3d1aa1cf10ffa7cfdb48b4814479c3c7101f24cc
MANPRIT <manprit.student.mat19@itbhu.ac.in>
2022-04-17 15:45:16 +0530

    my project on self help app

---
+++ app/src/main/java/net/penguincoders/doit/ChecklistFirstAidActivity.java
@@ -0,0 +1,14 @@
+package net.penguincoders.doit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChecklistFirstAidActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checklist_first_aid);
+    }
+}
